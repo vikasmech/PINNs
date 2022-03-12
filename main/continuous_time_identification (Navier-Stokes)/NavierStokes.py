f7f7cc5f233a05093a6c8f919ae0e3db54372728
vikas kumar <vikaskumar@strangeno.local>
2022-03-12 22:11:51 +0530

    convert it to run on tf1

--- main/continuous_time_identification (Navier-Stokes)/NavierStokes.py
+++ main/continuous_time_identification (Navier-Stokes)/NavierStokes.py
@@ -5,9 +5,8 @@
 import sys
 sys.path.insert(0, '../../Utilities/')
 
-#import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
@@ -21,7 +20,6 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.gridspec as gridspec
 
 np.random.seed(1234)
-tf.compat.v1.disable_eager_execution()
 tf.set_random_seed(1234)
 
 class PhysicsInformedNN:
